e085a206fbfbed7e8b2dffe2cd32921fb5610e32
kpc-simone <kpc.simone@gmail.com>
2023-06-13 16:12:26 -0400

    playing w/ various parameters

--- projects/fall-detection/scripts/lmu_fall_detection.py
+++ projects/fall-detection/scripts/lmu_fall_detection.py
@@ -18,8 +18,8 @@ subject_file = 'SA06-merged.csv'
 
 # LDN parameters
 size_in = 9         # 9 features of the fall detection data
-theta = 0.5
-q = 20
+theta = 1.
+q = 42
 
 # simulation parameters
 dt = 0.001
@@ -44,10 +44,10 @@ for idx in test_chunk_indices:
 
 train_xs = train_df[['AccX','AccY','AccZ','GyrX','GyrY','GyrZ','EulerX','EulerY','EulerZ']].to_numpy()
 lmu_train_xs = LDN(theta=theta, q=q, size_in=size_in).apply(train_xs)
-train_ys = train_df[['Fall/No Fall']].to_numpy()
+train_ys = train_df[['Fall/No Fall']].to_numpy() * 100
 
 test_xs = test_df[['AccX','AccY','AccZ','GyrX','GyrY','GyrZ','EulerX','EulerY','EulerZ']].to_numpy()
-test_ys = test_df[['Fall/No Fall']].to_numpy()
+test_ys = test_df[['Fall/No Fall']].to_numpy() * 100
 
 model = nengo.Network()
 with model:
@@ -55,17 +55,18 @@ with model:
     def stim_func(t):
         index = int(t/dt)-1
         return test_xs[index,:]
+        #return train_xs[index,:]
         
     stim = nengo.Node( size_out = size_in, output = stim_func )
     
     ldn = nengo.Node( LDN( theta = theta, q = q, size_in = size_in))
     nengo.Connection(stim, ldn, synapse=None)
     
-    neurons = nengo.Ensemble(n_neurons=200, dimensions=q*size_in, neuron_type=nengo.LIF())
+    neurons = nengo.Ensemble(n_neurons=1000, dimensions=q*size_in, neuron_type=nengo.LIFRate())
     nengo.Connection(ldn, neurons)
     
     # initialize the network with the training data
-    category = nengo.Ensemble(n_neurons=100,dimensions=1)
+    category = nengo.Ensemble(n_neurons=1000,dimensions=1,radius=100)
     nengo.Connection(neurons, category, eval_points=lmu_train_xs, function=train_ys)
     
     p_stim = nengo.Probe(stim)
@@ -74,9 +75,12 @@ with model:
 
 sim = nengo.Simulator(model,dt=dt)
 with sim:
-    sim.run(test_df.shape[0]*dt-1)
+    sim.run(test_df.shape[0]*dt)
+    #sim.run(train_df.shape[0]*dt)
     
 fig,ax = plt.subplots(1,1)
-ax.plot(sim.trange(), sim.data[p_category], label='output')
+ax.plot(sim.trange(), sim.data[p_category], label='Predicted')
+ax.plot(sim.trange(), test_ys.flatten(), label='True')
+#ax.plot(sim.trange(), train_ys.flatten(), label='True')
 ax.legend()
 plt.show()    
