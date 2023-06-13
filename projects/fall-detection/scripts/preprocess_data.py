c23bd3bee64c3bf8bd7839d23655da992bad466e
kpc-simone <kpc.simone@gmail.com>
2023-06-13 10:42:17 -0400

    fixed preprocess_data.py script

--- projects/fall-detection/scripts/preprocess_data.py
+++ projects/fall-detection/scripts/preprocess_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import sys,os
 
 sensor_data_dir = '../fall_detection_data/sensor_data'
@@ -20,31 +21,43 @@ for subject in subjects:
     lbdf['Task Code'] = 'T' + lbdf['Task Code (Task ID)'].str.split(' ',1,expand=True)[1].str.strip('()')
     lbdf['Trial Code'] = 'R0' + lbdf['Trial ID'].astype(str)
     lbdf['Sensor Data Code'] = lbdf['Subject Code'] + lbdf['Task Code'] + lbdf['Trial Code']
+    #print(lbdf['Fall_onset_frame'].dtype)
+    #print(lbdf['Fall_impact_frame'].dtype)
 
     # load the sensor data
     trial_files = [f for f in os.listdir(os.path.join(sensor_data_dir,subject)) if '.csv' in f]
     
     for i,trial_file in enumerate(trial_files):
         temp_df = pd.read_csv(os.path.join(sensor_data_dir,subject,trial_file))
+        #print(temp_df['FrameCounter'].dtype)
         
         # create a new dataframe for the individual if this is the first sensor data file we're grabbing
         if i == 0:
             ivdf = pd.DataFrame(columns = temp_df.columns)
             
         # grab the corresponding label data
-        trial_info = lbdf[ lbdf['Sensor Data Code'] == trial_file[:-4] ]
+        subject_code = trial_file[:-4]
+        subject_code = subject_code[:1] + 'A' + subject_code[1:]
+        trial_info = lbdf[ lbdf['Sensor Data Code'] == subject_code ].reset_index(drop=True)
         
         if trial_info.shape[0] > 0:
             
-            #fall_mask = (temp_df['Frame'] > trial_info['Fall_onset_frame']) & (temp_df['Frame'] < trial_info['Fall_impact_frame'])
-            temp_df['Fall/No Fall'] = np.where( (temp_df['Frame'] > trial_info['Fall_onset_frame']) & (temp_df['Frame'] < trial_info['Fall_impact_frame']), 1, 0)
-            temp_df['Fall Type'] = np.where( (temp_df['Frame'] > trial_info['Fall_onset_frame']) & (temp_df['Frame'] < trial_info['Fall_impact_frame']), trial_info['Task Code'], 0)
+            fall_onset_frame = trial_info['Fall_onset_frame'].iloc[0]
+            fall_impact_frame = trial_info['Fall_impact_frame'].iloc[0]
+            
+            temp_df['Fall/No Fall'] = np.where( (temp_df['FrameCounter'] > fall_onset_frame) & (temp_df['FrameCounter'] < fall_impact_frame), 1, 0)
+            temp_df['Fall Type'] = np.where( (temp_df['FrameCounter'] > fall_onset_frame) & (temp_df['FrameCounter'] < fall_impact_frame), trial_info['Task Code'], 0)
         else:
             temp_df['Fall/No Fall'] = 0
             temp_df['Fall Type'] = 0
         
-        print(temp_df[temp_df['Fall/No Fall'] == 1].sample(10))
         ivdf = pd.concat([ivdf,temp_df],axis=0,ignore_index=True)
+    
+    ivdf['Fall/No Fall'] = ivdf['Fall/No Fall'].astype(int)
+    ivdf['Fall Type'] = ivdf['Fall Type']#.astype(int)
+    
+    print(ivdf[ivdf['Fall/No Fall'] == 1].sample(10))
+    
     
     out_filename = '{}-merged.csv'.format(subject)
     ivdf.to_csv(os.path.join(out_dir,out_filename))
