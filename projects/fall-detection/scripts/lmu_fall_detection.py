c53e95b8c83d8885666958a195667a8957adde53
kpc-simone <kpc.simone@gmail.com>
2023-06-14 11:28:53 -0400

    performance metrics and project log

--- projects/fall-detection/scripts/lmu_fall_detection.py
+++ projects/fall-detection/scripts/lmu_fall_detection.py
@@ -1,6 +1,8 @@
 # lmu_classifier.py
 # Terrence C. Stewart's LMU implementation in Nengo
 
+from sklearn.metrics import confusion_matrix
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -24,11 +26,16 @@ q = 42
 # simulation parameters
 dt = 0.001
 
-# load data and generate the train/test split
-ddf = pd.read_csv(os.path.join(data_dir,subject_file),index_col=0).drop(['TimeStamp(s)','FrameCounter'],axis=1)
-
+# training dataset parameters
 train_test_split = 0.5
 chunk_size = 3600
+scaling_factor = 100
+
+# prediction parameters
+decision_threshold = 0.5
+
+# load data and generate the train/test split
+ddf = pd.read_csv(os.path.join(data_dir,subject_file),index_col=0).drop(['TimeStamp(s)','FrameCounter'],axis=1)
 
 chunk_indices = np.arange(0,ddf.shape[0],chunk_size)
 train_chunk_indices = np.random.choice(chunk_indices,size = int(len(chunk_indices)*train_test_split))
@@ -44,18 +51,17 @@ for idx in test_chunk_indices:
 
 train_xs = train_df[['AccX','AccY','AccZ','GyrX','GyrY','GyrZ','EulerX','EulerY','EulerZ']].to_numpy()
 lmu_train_xs = LDN(theta=theta, q=q, size_in=size_in).apply(train_xs)
-train_ys = train_df[['Fall/No Fall']].to_numpy() * 100
-
+train_ys = train_df[['Fall/No Fall']].to_numpy() * scaling_factor
 test_xs = test_df[['AccX','AccY','AccZ','GyrX','GyrY','GyrZ','EulerX','EulerY','EulerZ']].to_numpy()
-test_ys = test_df[['Fall/No Fall']].to_numpy() * 100
+test_ys = test_df[['Fall/No Fall']].to_numpy() * scaling_factor
 
 model = nengo.Network()
 with model:
 
     def stim_func(t):
         index = int(t/dt)-1
-        return test_xs[index,:]
-        #return train_xs[index,:]
+        #return test_xs[index,:]
+        return train_xs[index,:]
         
     stim = nengo.Node( size_out = size_in, output = stim_func )
     
@@ -66,7 +72,7 @@ with model:
     nengo.Connection(ldn, neurons)
     
     # initialize the network with the training data
-    category = nengo.Ensemble(n_neurons=1000,dimensions=1,radius=100)
+    category = nengo.Ensemble(n_neurons=1000,dimensions=1,radius=scaling_factor)
     nengo.Connection(neurons, category, eval_points=lmu_train_xs, function=train_ys)
     
     p_stim = nengo.Probe(stim)
@@ -75,12 +81,23 @@ with model:
 
 sim = nengo.Simulator(model,dt=dt)
 with sim:
-    sim.run(test_df.shape[0]*dt)
-    #sim.run(train_df.shape[0]*dt)
+    #sim.run(test_df.shape[0]*dt)
+    sim.run(train_df.shape[0]*dt)
     
-fig,ax = plt.subplots(1,1)
-ax.plot(sim.trange(), sim.data[p_category], label='Predicted')
-ax.plot(sim.trange(), test_ys.flatten(), label='True')
-#ax.plot(sim.trange(), train_ys.flatten(), label='True')
-ax.legend()
+predictions = np.where(train_ys.flatten() > np.max(train_ys.flatten())*decision_threshold, 1, 0)    
+print(predictions.dtype)
+print(train_ys.flatten().dtype)
+
+tn, fp, fn, tp = confusion_matrix( train_ys, predictions.flatten() ).ravel()
+    
+for performance_metric, number in zip(('True Negatives','False Positives','False Negatives','True Positives'),(tn, fp, fn, tp)):
+    print('{}: {}'.format(performance_metric,number))
+    
+fig,(ax1,ax2) = plt.subplots(2,1,sharex=True)
+ax1.plot(sim.trange(), train_ys.flatten(), label='True')
+ax2.plot(sim.trange(), sim.data[p_category]/scaling_factor, label='Predicted')
+ax2.plot(sim.trange(),predictions)
+
+ax1.legend()
+ax2.legend()
 plt.show()    
